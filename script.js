298dbc950bb575ebbb3475ac71140590bb6b9c62
Mustafa <mustafayousuf7777@gmail.com>
2024-09-17 17:24:34 -0400

    5

--- script.js
+++ script.js
@@ -45,21 +45,4 @@ document.addEventListener("DOMContentLoaded", function () {
     const images = document.querySelectorAll(".portfolio-item");
 
     images.forEach(img => {
-        img.addEventListener("click", () => {
-            modal.style.display = "block";
-            modalImg.src = img.src;
-            captionText.innerHTML = img.alt;
-        });
-    });
-
-    closeBtn.addEventListener("click", () => {
-        modal.style.display = "none";
-    });
-
-    // Close modal when clicking outside of the modal content
-    window.addEventListener("click", (event) => {
-        if (event.target === modal) {
-            modal.style.display = "none";
-        }
-    });
-});
+        img.addEvent
